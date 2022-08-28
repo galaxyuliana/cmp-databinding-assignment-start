60e4a8199edd9f97f8179549da5ddc6691193244
galaxyuliana <yulianashin613@gmail.com>
2022-08-27 23:39:25 -0700

    Added game-control component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { FormsModule } from '@angular/forms';
 
 
 import { AppComponent } from './app.component';
+import { GameControlComponent } from './game-control/game-control.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GameControlComponent
   ],
   imports: [
     BrowserModule,
