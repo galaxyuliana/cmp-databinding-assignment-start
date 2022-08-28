db8e373414fae627c9a532f1f66f2621ba386aae
galaxyuliana <yulianashin613@gmail.com>
2022-08-27 23:40:25 -0700

    Added even component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,14 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { GameControlComponent } from './game-control/game-control.component';
 import { OddComponent } from './odd/odd.component';
+import { EvenComponent } from './even/even.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     GameControlComponent,
-    OddComponent
+    OddComponent,
+    EvenComponent
   ],
   imports: [
     BrowserModule,
